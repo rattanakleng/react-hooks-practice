56d6c132a4b52fc85ad47138714627ba3a71ee90
Rattanak Leng <rleng@live.com>
2021-04-22 13:48:05 -0700

    update explaination

--- src/ImageToggleOnScroll.js
+++ src/ImageToggleOnScroll.js
@@ -1,34 +1,30 @@
-import React, {useEffect, useState, useRef} from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 
-const ImageToggleOnScroll = ({ primaryImg, secondaryImg}) => {
+const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
+  const imageRef = useRef(null)
 
-   const imageRef = useRef(null);
+  //The Element.getBoundingClientRect() method returns a DOMRect object providing information about the size of an element and its position relative to the viewport.
+  const isInView = () => {
+    const rect = imageRef.current.getBoundingClientRect()
+    return rect.top >= 0 && rect.bottom <= window.innerHeight
+  }
 
-   //Caculation base on image size compare to viewport
-   const isInView = () => {
-      const rect = imageRef.current.getBoundingClientRect();
-      return rect.top >= 0 && rect.bottom <= window.innerHeight;
-   }
+  const [inView, setInview] = useState(false)
 
-   const [inView, setInview] = useState(false);
+  useEffect(() => {
+    //Display element when window load
+    window.addEventListener('scroll', scrollHandler)
+    //Remove event listener when image out of full range in the view
+    return () => {
+      window.removeEventListener('scroll', scrollHandler)
+    }
+  }, [])
 
-   useEffect(() => {
-      //Display element when window load
-      window.addEventListener("scroll", scrollHandler);
-      //Remove event listener when image out of full range in the view
-      return () => {
-         window.removeEventListener("scroll", scrollHandler);
-      };
-   },[])
+  const scrollHandler = () => {
+    setInview(isInView())
+  }
 
-   const scrollHandler = () => {
-      setInview(isInView());
-   }
-
-   return (
-      <img src={inView ? secondaryImg : primaryImg}
-      alt="" ref={imageRef} />
-   )
+  return <img src={inView ? secondaryImg : primaryImg} alt="" ref={imageRef} />
 }
 
 export default ImageToggleOnScroll
